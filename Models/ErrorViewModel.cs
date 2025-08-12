a9a0c03c3d14e050cda66e28fa2dada9240fc1bc
Brandon Hill <bhilldev@outlook.com>
2025-08-12 12:09:07 -0500

    Initial commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace todo_app_asp_core.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
