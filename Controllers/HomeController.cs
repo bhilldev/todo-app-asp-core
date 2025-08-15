b534b252370eb26e8ec281bb375718730b1fb510
Brandon Hill <bhilldev@outlook.com>
2025-08-15 15:49:50 -0500

    Injected the model into the controller.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,13 @@
-using System.Diagnostics;
+//using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using todo_app_asp_core.Models;
+using TodoApp.Models;
 
 namespace todo_app_asp_core.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly TodoDbContext _context;
 
     public HomeController(ILogger<HomeController> logger)
     {
@@ -23,9 +24,9 @@ public class HomeController : Controller
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    //public IActionResult Error()
+    //{
+    //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    //}
 }
--- obj/Debug/net9.0/todo-app-asp-core.AssemblyInfo.cs
+++ obj/Debug/net9.0/todo-app-asp-core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todo-app-asp-core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32f54ba668b4ee4be1966689f5a59ce1bf9989f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2150af8480d3a5f3fa4193581ecb92ca88009b")]
 [assembly: System.Reflection.AssemblyProductAttribute("todo-app-asp-core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todo-app-asp-core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
